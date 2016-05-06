4eba8ffdc119ccdba51d701545ad8f5b6352a38d
Bob Holt <bobholt@gmail.com>
2016-05-06 11:28:37 -0400

    create default config overridden by ansible output

---
+++ flask_default_config.py
@@ -0,0 +1,7 @@
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = False
+TESTING = False
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
--- pythonserver/__init__.py
+++ pythonserver/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
+app.config.from_object('flask_default_config')
+app.config.from_object('flask_config')
+
 import pythonserver.views
